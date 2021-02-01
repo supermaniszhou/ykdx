5be11e7c07ce0cbfe46292c7cfbcc32cadffd2f3
supermaniszhou <741963634@qq.com>
2021-02-01 16:37:48 +0800

    add

---
+++ 1.医科大学-组织机构同步/src/com/seeyon/apps/ext/zxzyk/po/OrgMember.java
@@ -0,0 +1,172 @@
+package com.seeyon.apps.ext.zxzyk.po;
+
+/**
+ * Created by Administrator on 2019-7-29.
+ */
+public class OrgMember {
+    private String memberid;
+    private String membername;
+    private String loginName;
+    private String orgAccountId;
+    private String orgLevelId;
+    private String orgPostId;
+    private String orgDepartmentId;
+    private String membercode;
+    private Integer sortId = 1;
+    private Boolean enabled = true;
+    private String telNumber;
+    private String description;
+    private String ou;
+    private String p1;
+    private String p2;
+    private String p3;
+    private String p4;
+    private String p5;
+
+    public OrgMember() {
+    }
+
+    public String getMemberid() {
+        return memberid;
+    }
+
+    public void setMemberid(String memberid) {
+        this.memberid = memberid;
+    }
+
+    public String getMembername() {
+        return membername;
+    }
+
+    public void setMembername(String membername) {
+        this.membername = membername;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getOrgAccountId() {
+        return orgAccountId;
+    }
+
+    public void setOrgAccountId(String orgAccountId) {
+        this.orgAccountId = orgAccountId;
+    }
+
+    public String getOrgLevelId() {
+        return orgLevelId;
+    }
+
+    public void setOrgLevelId(String orgLevelId) {
+        this.orgLevelId = orgLevelId;
+    }
+
+    public String getOrgPostId() {
+        return orgPostId;
+    }
+
+    public void setOrgPostId(String orgPostId) {
+        this.orgPostId = orgPostId;
+    }
+
+    public String getOrgDepartmentId() {
+        return orgDepartmentId;
+    }
+
+    public void setOrgDepartmentId(String orgDepartmentId) {
+        this.orgDepartmentId = orgDepartmentId;
+    }
+
+    public String getMembercode() {
+        return membercode;
+    }
+
+    public void setMembercode(String membercode) {
+        this.membercode = membercode;
+    }
+
+    public Integer getSortId() {
+        return sortId;
+    }
+
+    public void setSortId(Integer sortId) {
+        this.sortId = sortId;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public void setTelNumber(String telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOu() {
+        return ou;
+    }
+
+    public void setOu(String ou) {
+        this.ou = ou;
+    }
+
+    public String getP1() {
+        return p1;
+    }
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+    public String getP2() {
+        return p2;
+    }
+
+    public void setP2(String p2) {
+        this.p2 = p2;
+    }
+
+    public String getP3() {
+        return p3;
+    }
+
+    public void setP3(String p3) {
+        this.p3 = p3;
+    }
+
+    public String getP4() {
+        return p4;
+    }
+
+    public void setP4(String p4) {
+        this.p4 = p4;
+    }
+
+    public String getP5() {
+        return p5;
+    }
+
+    public void setP5(String p5) {
+        this.p5 = p5;
+    }
+}
