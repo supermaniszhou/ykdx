718c3348b63eccff49ca6fd320d438c27b92091f
supermaniszhou <741963634@qq.com>
2021-02-08 10:50:22 +0800

    update

--- 1.医科大学-组织机构同步/src/com/seeyon/apps/ext/zxzyk/manager/OrgLevelManager.java
+++ 1.医科大学-组织机构同步/src/com/seeyon/apps/ext/zxzyk/manager/OrgLevelManager.java
@@ -1,11 +1,13 @@
 package com.seeyon.apps.ext.zxzyk.manager;
 
 
+import java.sql.SQLException;
+
 public interface OrgLevelManager {
 
-    void insertOrgLevel();
+    void insertOrgLevel() throws SQLException;
 
-    void updateOrgLevel();
+    void updateOrgLevel() throws SQLException;
 
-    void deleteNotExistLevel();
+    void deleteNotExistLevel() throws SQLException;
 }
--- 1.医科大学-组织机构同步/src/com/seeyon/apps/ext/zxzyk/manager/OrgLevelManagerImpl.java
+++ 1.医科大学-组织机构同步/src/com/seeyon/apps/ext/zxzyk/manager/OrgLevelManagerImpl.java
@@ -4,6 +4,7 @@ import com.seeyon.apps.ext.zxzyk.dao.OrgLevelDao;
 import com.seeyon.apps.ext.zxzyk.dao.OrgLevelDaoImpl;
 import com.seeyon.apps.ext.zxzyk.po.OrgLevel;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class OrgLevelManagerImpl implements OrgLevelManager {
@@ -11,19 +12,19 @@ public class OrgLevelManagerImpl implements OrgLevelManager {
     private OrgLevelDao orgLevelDao = new OrgLevelDaoImpl();
 
     @Override
-    public void insertOrgLevel() {
+    public void insertOrgLevel() throws SQLException {
         List<OrgLevel> list = orgLevelDao.queryOrgLevel();
         orgLevelDao.insertOrgLevel(list);
     }
 
     @Override
-    public void updateOrgLevel() {
+    public void updateOrgLevel() throws SQLException {
         List<OrgLevel> list = orgLevelDao.queryChangerLevel();
         orgLevelDao.updateOrgLevel(list);
     }
 
     @Override
-    public void deleteNotExistLevel() {
+    public void deleteNotExistLevel() throws SQLException {
         List<OrgLevel> list = orgLevelDao.queryNotExistLevel();
         orgLevelDao.deleteOrgLevel(list);
     }
